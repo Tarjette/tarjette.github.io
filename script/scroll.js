03a3db0071d2c0d32665a1d21b02fe04fdea2753
Léo PIREZé <hadx77@gmail.com>
2024-06-01 20:11:33 +0200

    add all

--- script/scroll.js
+++ script/scroll.js
@@ -1,12 +1,14 @@
 const headerButton = document.getElementById("goto-header-button");
 const aproposButton = document.getElementById("goto-a-propos-button");
 const formationButton = document.getElementById("goto-formation-button");
+const skillsButton = document.getElementById("goto-skills-button");
 const creaButton = document.getElementById("goto-crea-button");
 const contactButton = document.getElementById("goto-contact-button");
 
 createButtonScroll(headerButton, "header-animated-canva");
 createButtonScroll(aproposButton, "a-propos-section");
 createButtonScroll(formationButton, "formation-section");
+createButtonScroll(skillsButton, "skills-section");
 createButtonScroll(creaButton, "creation-section");
 createButtonScroll(contactButton, "contact");
 
